3431ce7d9680bd50d529f6a92685a94780720ba5
Mohit Arun Inampudi <mi9.developer@gmail.com>
2017-10-07 16:39:41 -0400

    Initial commit

---
+++ src/main/java/helloworld/demo/HelloWorldWS.java
@@ -0,0 +1,14 @@
+package helloworld.demo;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class HelloWorldWS {
+	
+	@WebMethod
+	public String sayHello() {
+		return "Hello World";
+		
+	}
+
+}
